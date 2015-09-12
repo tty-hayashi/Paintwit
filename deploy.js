6c672e3760207905acb7ecd5426f7fa739f80678
tetha <tetha@mac.com>
2015-09-12 10:16:14 +0900

    initial

---
+++ deploy.js
@@ -0,0 +1,8 @@
+define([
+    'amber/deploy',
+    // --- packages to be deployed begin here ---
+    'amber-paintwit/Paintwit'
+    // --- packages to be deployed end here ---
+], function (amber) {
+    return amber;
+});
---
+++ src/Paintwit-Tests.js
@@ -0,0 +1,9 @@
+define("amber-paintwit/Paintwit-Tests", ["amber/boot", "amber_core/SUnit"], function($boot){"use strict";
+var $core=$boot.api,nil=$boot.nil,$recv=$boot.asReceiver,$globals=$boot.globals;
+$core.addPackage('Paintwit-Tests');
+$core.packages["Paintwit-Tests"].innerEval = function (expr) { return eval(expr); };
+$core.packages["Paintwit-Tests"].transport = {"type":"amd","amdNamespace":"amber-paintwit"};
+
+$core.addClass('PaintwitTest', $globals.TestCase, [], 'Paintwit-Tests');
+
+});
